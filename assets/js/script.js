9672449a145f258152a242f05dc68eb4c2437405
Cauã(euzin) <cauathome3@gmail.com>
2023-02-08 20:57:50 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/*
+let x = 10;
+let y = 5;
+console.log(x > y);
+let w = "10";
+let z = 10;
+console.log(w < z)
+let a = 10;
+let b = 5;
+console.log(a > 5)
+
+
+let preco = 46;
+if (preco <= 45) {
+    console.log("a carne ta barata")
+}else if (preco > 45) {
+    console.log("A carne ta cara")
+}
+
+let age =90;
+
+let isAdult = (age>=18 ?  'true' : 'false');
+console.log(isAdult)
+*/
+
+/* função que retorna um dado especifico*/
+/* cria uma função 
+dentro dela vai fazer as coisas
+ é um codigo que só vai ser executado quando eu quiser q for executado
+*/
+
+/*
+function pinto() {
+    console.log('meu pinto é pequeno...')
+    console.log('(euacho)')
+}
+*/
+
+/*basta colocar o  nome da function com o parenteses tipo:
+function pinto() {
+    console.log('meu pinto é pequeno...')
+    console.log('(euacho)')
+}
+
+pinto()
+
+*/
+
